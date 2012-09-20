e44940458b0253893fa4b3bb7a18a68e6bb15dab
korzun1993 <korzun1993@mail.ru>
2012-09-20 18:13:12 +0300

    Initial Commit

---
+++ OstaninTask/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  OstaninTask
+//
+//  Created by Корзун Владислав on 20.09.12.
+//  Copyright (c) 2012 Корзун Владислав. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ OstaninTask/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OstaninTask
+//
+//  Created by Корзун Владислав on 20.09.12.
+//  Copyright (c) 2012 Корзун Владислав. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
